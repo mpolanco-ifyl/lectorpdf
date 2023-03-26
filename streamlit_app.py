2830a6dd15443ffa64e12816c1dc716803a647ad
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-03-25 18:50:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pdfminer.high_level import extract_text
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import requests
 from io import BytesIO
 
@@ -9,14 +9,13 @@ def extract_text_from_pdf(file):
     return text
 
 def answer_question(question, context):
-    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-    model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
-    inputs = tokenizer(question, context, return_tensors="pt")
-    outputs = model(**inputs)
-    answer_start = outputs.start_logits.argmax().item()
-    answer_end = outputs.end_logits.argmax().item() + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
-    return answer
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    input_text = f"responder la pregunta: '{question}' basada en el siguiente contexto: {context}"
+    input_tokens = tokenizer.encode(input_text, return_tensors='pt')
+    output_tokens = model.generate(input_tokens, max_length=200, num_return_sequences=1)
+    output_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+    return output_text
 
 st.title("Responder preguntas a partir de un PDF")
 uploaded_file = st.file_uploader("Carga un archivo PDF", type=['pdf'])
@@ -24,7 +23,6 @@ uploaded_file = st.file_uploader("Carga un archivo PDF", type=['pdf'])
 if uploaded_file is not None:
     with st.spinner('Extrayendo texto del PDF...'):
         text = extract_text_from_pdf(BytesIO(uploaded_file.read()))
- 
 
     user_question = st.text_input("Escribe tu pregunta sobre el contenido del PDF:")
     if user_question:
